508549b9bb943e1338198687aa8301b933ed4e2a
vindhya536 <k.vindhyareddy536@gmail.com>
2016-08-30 11:48:44 +0530

    ssr welcome selenium

---
+++ src/seliniumprojects1/Log4j.java
@@ -0,0 +1,10 @@
+package seliniumprojects1;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Log4j {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+FirefoxDriver  fr=new FirefoxDriver();
+fr.get();('H++P'//amazon )
